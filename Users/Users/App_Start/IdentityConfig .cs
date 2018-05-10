24c70e8c2aaf28a5b337af1be5d6a062aa346615
moxiaxue <mochou123456@163.com>
2018-05-10 02:23:27 -0500

    Initialize basic setup:  1. Create DB-->IdentityDb  2. "Seed" data via EF(Code first)  Create User class --> (AppUser:IdenityUser)            Create Db Context-->(AppIdentityDbContext:IdentityDbContext<AppUser>)            Create User Manager-->(AppUserManager:UserManger<AppUser>)   3. Creating Start Class

---
+++ Users/Users/App_Start/IdentityConfig .cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Users.Infrastructure;
+
+namespace Users.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
---
+++ Users/Users/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        //connect with database
+        public AppIdentityDbContext():base("IdentityDb"){}
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    //when the schema is first created through the Entity Framework Code First feature,this class will be called
+    public class IdentityDbInit:DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+
+        }
+    }
+}
