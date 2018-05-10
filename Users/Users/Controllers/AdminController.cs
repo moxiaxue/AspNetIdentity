d07b1bb22cc3c6d5ce7b25e515a9bec9c9901eeb
moxiaxue <mochou123456@163.com>
2018-05-11 00:47:42 +0800

    Add the Del/Edit

--- Users/Users/Controllers/AdminController.cs
+++ Users/Users/Controllers/AdminController.cs
@@ -45,6 +45,64 @@ namespace Users.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Delete(String id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    return View("Error", result.Errors);
+            }
+            else
+                return View("Error", new string[] { "User Not Found" });
+        }
+
+
+        public async Task<ActionResult> Edit(String id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null) return View(user);
+            else return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(String id,String email, String password)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if(user!=null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await UserManager.UserValidator.ValidateAsync(user);
+                if (!validEmail.Succeeded)
+                    AddErrorsFromResult(validEmail);
+
+                IdentityResult validPassword = null;
+                if(password!=String.Empty)
+                {
+                    validPassword = await UserManager.PasswordValidator.ValidateAsync(password);
+                    if (validPassword.Succeeded)
+                        user.PasswordHash = UserManager.PasswordHasher.HashPassword(password);
+                    else
+                        AddErrorsFromResult(validPassword);
+                }
+
+                if((validEmail.Succeeded&&validPassword==null)||password!=String.Empty&&validPassword.Succeeded)
+                {
+                    IdentityResult result = await UserManager.UpdateAsync(user);
+                    if (result.Succeeded) return RedirectToAction("Index");
+                    else
+                        AddErrorsFromResult(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach(String error in result.Errors)
