a9e79359f8d964c0ef7e1458b199efcd2cf0c003
moxiaxue <mochou123456@163.com>
2018-05-29 16:51:54 +0800

    List out declaration infomation

---
+++ Users/Users/Controllers/ClaimsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Users.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        // GET: Claims
+        [Authorize]
+        public ActionResult Index()
+        {
+            ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
+            if (ident == null)
+                return View("Error", new string[] { "No claims available" });
+            else
+                return View(ident.Claims);
+        }
+    }
+}
--- Users/Users/Infrastructure/IdentityHelpers.cs
+++ Users/Users/Infrastructure/IdentityHelpers.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +15,17 @@ namespace Users.Infrastructure
         {
             AppUserManager mgr = HttpContext.Current.GetOwinContext().GetUserManager<AppUserManager>();
             return new MvcHtmlString(mgr.FindByIdAsync(id).Result.UserName);
+        }
+
+        public static MvcHtmlString ClaimType(this HtmlHelper html,String claimType)
+        {
+            FieldInfo[] fields = typeof(ClaimTypes).GetFields();
+            foreach(FieldInfo field in fields)
+            {
+                if (field.GetValue(null).ToString() == claimType)
+                    return new MvcHtmlString(field.Name);
+            }
+            return new MvcHtmlString(String.Format("{0}", claimType.Split('/', '.').Last()));
         }
     }
 }
